e81c2240e7203a020cd8e968fa0cc2c7ff0dbf2c
yash52003 <yashchoudhary52003@gmail.com>
2024-06-03 18:25:05 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/' , (req , res) => {
+    res.json({
+        message : "We are on the main route"
+    })
+})
+
+app.get('/home' , (req , res) => {
+    res.json({
+        message : "We are on the  home route"
+    })
+})
+
+app.listen(3000 , () => {
+    console.log("App is listening on the PORT:3000");
+})
